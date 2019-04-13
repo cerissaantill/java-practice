ac6346f06f06d211efe0553383fda3dc87bcced7
Cerissa Antill <cerissa.antill@gmail.com>
2019-04-13 16:51:50 -0500

    add age restriction conditional, exit code, & global system object

--- src/TreeStory.java
+++ src/TreeStory.java
@@ -1,10 +1,28 @@
 import java.util.Scanner;
-
+import java.lang.String;
 
 public class TreeStory {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("\nHow old are you? ");
+        String ageAsString = scanner.nextLine();
+
+
+        // age restriction conditional using parseInt
+        int age = Integer.parseInt(ageAsString);
+
+        if (age < 13) {
+            // insert exit code
+            System.out.print("Sorry, you must be at lease 13 to use this program.\n");
+
+            // global system object
+            System.exit(0);
+        }
+
+
 
         System.out.print("\nEnter your name: ");
         String name = scanner.nextLine();
