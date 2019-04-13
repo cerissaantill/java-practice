880d592d20011db0f28c44853ef38f98571c5bd3
Cerissa Antill <cerissa.antill@gmail.com>
2019-04-13 16:25:14 -0500

    added to madlibs treestory exercise

--- src/TreeStory.java
+++ src/TreeStory.java
@@ -12,7 +12,17 @@ public class TreeStory {
         System.out.print("\nEnter an adjective: ");
         String adjective = scanner.nextLine();
 
-        System.out.printf("\n%s is very %s.", name, adjective);
+        System.out.print("\nEnter a noun: ");
+        String noun = scanner.nextLine();
+
+        System.out.print("\nEnter an adverb: ");
+        String adverb = scanner.nextLine();
+
+        System.out.print("\nEnter a verb ending in -ing: ");
+        String verb = scanner.nextLine();
+
+        System.out.print("\nYour TreeStory: \n--------------------------\n");
+        System.out.printf("\n%s is a %s %s. They are always %s %s.", name, adjective, noun, adverb, verb);
 
 
     } // main method
