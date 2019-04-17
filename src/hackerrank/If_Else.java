8d5a5d6b6f04983aa76fb6e87ffb2f67dadb3a61
Cerissa Antill <cerissa.antill@gmail.com>
2019-04-17 11:50:57 -0500

    correct answer

--- src/hackerrank/If_Else.java
+++ src/hackerrank/If_Else.java
@@ -19,23 +19,23 @@ public class If_Else {
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int N = scanner.nextInt();
+        int n = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
 
-        if ((n % 1) == 1) {
+        if ((n % 2) == 1) {
             System.out.print("Weird");
 
-        } else if {
-            (n >= 2 && n <= 5);
+        } else if
+            (n >= 2 && n <= 5) {
             System.out.print("Not Weird");
 
-        } else if {
-            (n >= 6 && n <= 20);
+        } else if
+            (n >= 6 && n <= 20) {
             System.out.print("Weird");
 
-        } else if {
-            (n % 2 = 0 && n > 20);
+        } else if
+            ((n % 2 == 0) && n > 20) {
             System.out.print("Not Weird");
         }
 
