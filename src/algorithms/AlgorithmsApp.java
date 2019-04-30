a1639123d3184407c4536a948e296cf4dca7c27f
Cerissa Antill <cerissa.antill@gmail.com>
2019-04-30 10:25:26 -0500

    unfinished answer to the mock interview question

---
+++ src/algorithms/AlgorithmsApp.java
@@ -0,0 +1,19 @@
+package algorithms;
+
+import java.util.Arrays;
+
+public class AlgorithmsApp {
+
+
+    public static void main(String[] args) {
+        int[] numbers = {25, 7, 77, 16, 83, 007, 5644, 84, 777, 9184};
+
+
+
+    }
+
+}
+
+
+
+}
---
+++ src/algorithms/SelectionSort.java
@@ -0,0 +1,26 @@
+package algorithms;
+
+public class SelectionSort implements Sort {
+
+    @Override
+    public int[] sort (int[] numbers) {
+
+        int[] sorted = new int[numbers.length];
+
+        // loops over the items and selects the lowest
+        for (int i = 1; i < numbers.length; i++) {
+            int nextLowest = numbers[0];
+
+            // do this for each item in the list
+            for(int j = 0; j < numbers.length; j++) {
+                if (numbers[j] < nextLowest && numbers[j] > sorted[i -1])
+                    nextLowest = numbers[i];
+            }
+            sorted[i]
+
+        }
+
+
+    }
+
+}
