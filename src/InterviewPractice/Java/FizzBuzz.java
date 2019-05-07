39635fa9b11e997471e4d6a441a1b0be17defe5d
Cerissa Antill <cerissa.antill@gmail.com>
2019-05-07 14:38:57 -0500

    spacing

--- src/InterviewPractice/Java/FizzBuzz.java
+++ src/InterviewPractice/Java/FizzBuzz.java
@@ -7,7 +7,6 @@ public class FizzBuzz {
     public static void main(String[] args) {
 
         for (int i = 1; i <= 100; i++) {
-
             if (i % 3 == 0 && i % 5 == 0) {
                 System.out.println("fizz buzz");
 
