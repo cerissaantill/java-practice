f0c16e3c48ba7ac4847407de04606d3f41a48538
Cerissa Antill <cerissa.antill@gmail.com>
2019-05-07 14:31:51 -0500

    answered fizzbuzz in java

---
+++ src/InterviewPractice/Java/FizzBuzz.java
@@ -0,0 +1,23 @@
+package InterviewPractice.Java;
+import java.lang.String;
+import java.util.*;
+
+
+public class FizzBuzz {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("fizz buzz");
+            } else if (i % 5 == 0) {
+                System.out.println("buzz");
+            } else if (i % 3 == 0) {
+                System.out.println("fizz");
+            }
+
+        }  // for loop
+
+
+    }  // main method
+}  // FizzBuzz class
