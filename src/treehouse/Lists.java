943c66a55288582be3e94b7fc8dad50206a0321e
Cerissa Antill <cerissa.antill@gmail.com>
2019-05-06 21:12:15 -0500

    working on Lists class

---
+++ src/treehouse/Lists.java
@@ -0,0 +1,19 @@
+package treehouse;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class Lists {
+
+    public static void main(String[] args) {
+        List<String> groceryLine = new ArrayList<>();
+        groceryLine.add("Jerome");
+        groceryLine.add("Beth");
+        groceryLine.add("Sam");
+
+        System.out.println(groceryLine);
+
+    }  // main method
+}  // Lists class
