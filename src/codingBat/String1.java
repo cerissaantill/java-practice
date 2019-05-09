7e70083e7149d3ed45a1a3329bbf665e1061fee0
Cerissa Antill <cerissa.antill@gmail.com>
2019-05-08 22:28:56 -0500

    completed helloName

---
+++ src/codingBat/String1.java
@@ -0,0 +1,16 @@
+package codingBat;
+
+public class String1 {
+
+/**  Given a string name, e.g. "Bob", return a greeting in the form of "Hello Bob!" */
+
+public String helloName(String name)  {
+    return("Hello " + name + "!");
+}
+
+
+
+
+
+
+}  // String1 class
