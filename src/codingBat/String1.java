938829c709563ce78cc18953cd98e64bcee695d5
Cerissa Antill <cerissa.antill@gmail.com>
2019-05-08 22:49:04 -0500

    completed 1-4 on String-1

--- src/codingBat/String1.java
+++ src/codingBat/String1.java
@@ -1,14 +1,43 @@
 package codingBat;
+import java.lang.String;
 
 public class String1 {
 
-/**  Given a string name, e.g. "Bob", return a greeting in the form of "Hello Bob!" */
 
+//  Given a string name, e.g. "Bob", return a greeting in the form of "Hello Bob!"
 public String helloName(String name)  {
     return("Hello " + name + "!");
 }
 
 
+
+// Given two strings, a and b, return the result of putting them together in the order abba,
+//   e.g. "Hi" and "Bye" returns "HiByeByeHi".
+
+public String makeAbba(String a, String b) {
+    return (a+b+b+a);
+}
+
+
+
+//  The web is built with HTML Strings like "<i>Yay</i>" which draws Yay as italic text.
+//  In this example, the "i" tag makes <i> and </i> which surround the word "Yay".
+//  Given tag and word strings, create the HTML string with tags around the word,
+//      e.g. "<i>Yay</i>"
+public String makeTags(String tag, String word) {
+    return ('<' + tag + '>' + word + '<' + '/' + tag + '>');
+}
+
+
+
+//  Given an "out" string length 4, such as "<<>>", and a word, return a new string
+//  where the word is in the middle of the out string, e.g. "<<word>>".
+//  Note: use str.substring(i,j) to extract the String starting at index i and going up to but
+//  not including index j.
+
+public String makeOutWord(String out, String word) {
+    return (out.substring(0, 2) + word + out.substring(2, 4));
+}
 
 
 
