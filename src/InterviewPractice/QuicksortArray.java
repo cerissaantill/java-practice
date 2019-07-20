1ff08fc70ea81d3ec46425027af2db2d1182c627
Cerissa Antill <cerissa.antill@gmail.com>
2019-07-20 16:30:55 -0500

    solving using quicksort

---
+++ src/InterviewPractice/QuicksortArray.java
@@ -0,0 +1,13 @@
+package InterviewPractice;
+
+import java.util.Arrays;
+
+public class QuicksortArray {
+
+    public static void main(String[] args) {
+
+        int [] unsortedArr = {10,8,7,3,2,1,5,4,6,9};
+
+        System.out.println("Unsorted array: " + Arrays.toString(unsortedArr));
+    }
+}
--- src/InterviewPractice/reverseString.java
+++ src/InterviewPractice/reverseString.java
@@ -2,7 +2,7 @@ package InterviewPractice;
 
 public class reverseString {
 
-    // Reverse a String:
+    // Reverse a String using reverse iteration:
 
 
 
@@ -11,11 +11,10 @@ public class reverseString {
 
         String reverse = "cerissa";
 
-    for(int i >= 0; int i = reverse.length() -1; i--) {
-        reverse = reverse + reverse.charAt(i);
-    }
-        System.out.println("The reversed string is: ");
-        System.out.println(reverse);
+        for (int i = reverse.length()-1; i--) {
+            reverse = reverse + reverse.charAt(i);
+        }
+            System.out.println(reverse);
 
 
 
