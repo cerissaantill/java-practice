8ec38a2f53b1bf7fb1b2d3a4ba461c0c00d61c97
Cerissa Antill <cerissa.antill@gmail.com>
2019-07-20 15:15:23 -0500

    working on stringReverse

---
+++ src/InterviewPractice/reverseString.java
@@ -0,0 +1,24 @@
+package InterviewPractice;
+
+public class reverseString {
+
+    // Reverse a String:
+
+
+
+
+    public static void main(String[] args) {
+
+        String reverse = "cerissa";
+
+    for(int i >= 0; int i = reverse.length() -1; i--) {
+        reverse = reverse + reverse.charAt(i);
+    }
+        System.out.println("The reversed string is: ");
+        System.out.println(reverse);
+
+
+
+
+    } // main
+}
